ceaf45bc699b1e0e3dcb7fbd3ada4bd065d0c4c4
Arc1llusion <arc1llusion@gmail.com>
2019-01-08 17:59:05 -0500

    a

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,10 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
     [SerializeField] GameObject projectile, gun;
+
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if(IsAttackerInLane())
+        {
+            animator.SetBool("IsAttacking", true);   
+        }
+        else
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        var spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(var spawner in spawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+
+            if(isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        return true;
+    }
 
     public void Fire()
     {
