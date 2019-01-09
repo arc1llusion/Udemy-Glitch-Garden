ffab31de988299e27066fa1bb1c4c028a23773a9
Arc1llusion <arc1llusion@gmail.com>
2019-01-09 17:31:24 -0500

    To Lecture 157

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.GetComponent<Defender>() != null)
+        {
+            if (otherObject.GetComponent<Gravestone>() != null)
+            {
+                GetComponent<Animator>().SetTrigger("JumpTrigger");
+            }
+            else
+            {
+                GetComponent<Attacker>().Attack(otherObject);
+            }
+        }
+    }
+}
